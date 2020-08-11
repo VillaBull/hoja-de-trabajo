76b9fb48df2520034d93566ef0be8f64acfcd907
Marcos Villatoro <marcosdanii27@gmail.com>
2020-08-11 13:10:33 -0600

    realicé el ingreso de los datos para ambas fracciones

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace hoja_de_trabajo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numerador = 0, Denominador = 1;
+            int Numerador2 = 0, Denominador2 = 1;
+            Console.Write("Num 1: ");
+            Numerador = Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("     ----");
+            Console.Write("Den 1: ");
+            Denominador = Convert.ToInt32( Console.ReadLine());
+
+
+            Console.Write("Num 2: ");
+            Numerador2 = Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("     ----");
+            Console.Write("Den 2: ");
+            Denominador2 = Convert.ToInt32( Console.ReadLine());
+        }
+    }
+}
