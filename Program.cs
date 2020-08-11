9d6b9aea204468644158ed4df1d5f487c30a84ae
Marcos Villatoro <marcosdanii27@gmail.com>
2020-08-11 14:04:26 -0600

    Agregué algunas lineas de codigo para que se entendiera mas

--- Program.cs
+++ Program.cs
@@ -8,18 +8,21 @@ namespace hoja_de_trabajo
         {
             int Numerador = 0, Denominador = 1;
             int Numerador2 = 0, Denominador2 = 1;
-            Console.Write("Num 1: ");
+
+            Console.WriteLine("Ingrese Primer Fracción");
+            Console.Write("Num: ");
             Numerador = Convert.ToInt32( Console.ReadLine());
-            Console.WriteLine("     ----");
-            Console.Write("Den 1: ");
+            Console.WriteLine("    ----");
+            Console.Write("Den: ");
             Denominador = Convert.ToInt32( Console.ReadLine());
 
             Console.Write("");
 
-            Console.Write("Num 2: ");
+            Console.WriteLine("Ingrese Segunda Fracción");
+            Console.Write("Num: ");
             Numerador2 = Convert.ToInt32( Console.ReadLine());
-            Console.WriteLine("     ----");
-            Console.Write("Den 2: ");
+            Console.WriteLine("    ----");
+            Console.Write("Den: ");
             Denominador2 = Convert.ToInt32( Console.ReadLine());
 
             Console.ReadKey();
